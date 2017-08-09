c9e38f5d1d083d363020500a660e66741898a9db
victor <lau.victor.w@gmail.com>
2017-08-09 16:14:34 -0700

    changed port to dynamic port and set node PORT

--- server/server.js
+++ server/server.js
@@ -4,10 +4,11 @@ var axios = require('axios');
 
 var app = express();
 
-app.use(express.static(__dirname + '/../react-client/dist'));
+let port = process.env.PORT || 3000
+app.use(express.static(__dirname + '/../src/client/public'));
 
 
 
-app.listen(3000, function() {
-   console.log("listening on port 3000!");
+app.listen(port, function() {
+   console.log(`listening on port ${port}`);
 });
