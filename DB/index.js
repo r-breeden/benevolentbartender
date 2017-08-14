b45dd15c6558d58cdeccd5df7a5bc7fe5070ba9c
Adisa Stewart <adisa.stewart1988@gmail.com>
2017-08-14 16:19:55 -0700

    create db connect and db query

---
+++ DB/index.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql');
+const mysqlConfig = require('./config');
+
+var connection = mysql.createConnection(mysqlConfig);
+
+var getRecipes = (ingStr, cb) => {
+  //split string
+  var ingArr = ingStr.split(',');
+  //create query string
+  var queryStr = `SELECT * FROM recipes WHERE ingStr LIKE %${ingArr[0]}%`;
+  for (var i = 1; i < ingArr.length; i++) {
+    queryStr += ` AND ingStr LIKE %${ingArr[i]}%`; 
+  }
+  //query db
+  connection.query(queryStr, (err, results) => {
+    if (err) {
+      cb(err, null);
+    } else {
+      cb(null, results);
+    }
+  });
+};
+
+module.exports = {getRecipes};
