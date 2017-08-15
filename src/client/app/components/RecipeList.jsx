66f9a546da7cce5199afbcce606201faf8281285
Ryan Breeden <ryan@ryanbreeden.com>
2017-08-15 11:22:44 -0700

    typo fix

--- src/client/app/components/RecipeList.jsx
+++ src/client/app/components/RecipeList.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { RecipeCar } from './RecipeCard.jsx';
+import { RecipeCard } from './RecipeCard.jsx';
 
 
 export class RecipeList extends React.Component{
