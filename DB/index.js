4f0a853c5ef70a123b2a4b9b22f1a521e953eea6
Adisa Stewart <adisa.stewart1988@gmail.com>
2017-08-15 17:33:29 -0700

    add db query methods

--- DB/index.js
+++ DB/index.js
@@ -7,9 +7,9 @@ var getRecipes = (ingStr, cb) => {
   //split string
   var ingArr = ingStr.split(',');
   //create query string
-  var queryStr = `SELECT * FROM recipes WHERE ingStr LIKE %${ingArr[0]}%`;
+  var queryStr = `SELECT * FROM recipes WHERE ingStr LIKE '%${ingArr[0]}%'`;
   for (var i = 1; i < ingArr.length; i++) {
-    queryStr += ` AND ingStr LIKE %${ingArr[i]}%`; 
+    queryStr += ` AND ingStr LIKE '%${ingArr[i]}%'`; 
   }
   //query db
   connection.query(queryStr, (err, results) => {
@@ -23,7 +23,7 @@ var getRecipes = (ingStr, cb) => {
 
 var grabNamedRecipe = (name, cb) => {
 
-  connection.query(`SELECT * FROM recipes WHERE name = ${name}`, (err, results) => {
+  connection.query(`SELECT * FROM recipes WHERE name = '${name}'`, (err, results) => {
     if (err) {
       cb(err, null);
     } else {
@@ -33,4 +33,64 @@ var grabNamedRecipe = (name, cb) => {
 
 };
 
-module.exports = {getRecipes, grabNamedRecipe};
+var addIngredient = (name) => {
+
+  connection.query(`INSERT INTO ingredients (name) VALUES ('${name}')`, (err, results) => {
+    if (err) {
+      console.log(err, null);
+    } else {
+      console.log(null, results);
+    }
+  });
+
+};
+
+// duplicates should be handled by unique contraint on name field?
+var addRecipe = (recipeObj) => {
+
+  var queryStr = `INSERT INTO recipes (name, instructions, ingredients, measurements, imageUrl) 
+  VALUES ('${recipeObj.name}', '${recipeObj.instructions}', '${recipeObj.ingredients}', '${recipeObj.measurements}', '${recipeObj.imageUrl}')`;
+
+  connection.query(queryStr, (err, results) => {
+    if (err) {
+      console.log(err, null);
+    } else {
+      console.log(null, results);
+    }
+  });
+
+};
+
+//assumes input of a string(name) & an object with properties named for fields to be updated w/ the values to replace current
+//can be updated later if this needs to be changed
+var editRecipe = (name, fieldObj) => {
+  //create query string
+  var queryStr = `UPDATE recipes SET `;
+  for (var key in fieldObj) {
+    queryStr += `${key} = '${fieldObj[key]}', `;
+  }
+  var completeQuery = `${queryStr.slice(0, -2)} WHERE name = '${name}'`;
+
+  connection.query(completeQuery, (err, results) => {
+    if (err) {
+      console.log(err, null);
+    } else {
+      console.log(null, results);
+    }
+  }); 
+
+};
+
+var deleteRecipe = (name) => {
+
+  connection.query(`DELETE FROM recipes WHERE name = ${name}`, (err, results) => {
+    if (err) {
+      console.log(err, null);
+    } else {
+      console.log(null, results);
+    }
+  });
+
+};
+
+module.exports = {getRecipes, grabNamedRecipe, addIngredient, addRecipe, editRecipe, deleteRecipe};
